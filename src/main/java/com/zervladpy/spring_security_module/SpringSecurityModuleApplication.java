0815c79dd1961083f6a7b0020b0322a91e625ff8
zervladpy <zernovvladislav7@gmail.com>
2024-09-03 12:48:50 +0200

    initial

---
+++ src/main/java/com/zervladpy/spring_security_module/SpringSecurityModuleApplication.java
@@ -0,0 +1,13 @@
+package com.zervladpy.spring_security_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zervladpy/spring_security_module/SpringSecurityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zervladpy.spring_security_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
